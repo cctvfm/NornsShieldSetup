10ac7bb0e4a30e380d3e140b3851dce526d825db
cctvfm <fil@ptrz.ca>
2020-06-04 10:46:04 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Norns Setup
+echo "Expanding Filesystem"
+raspi-config --expand-rootfs
+echo "Success"
+echo "Cloning Scripts
+cd ~/dust/code
+git clone https://github.com/justmat/phyllis.git
+git clone https://github.com/justmat/showers.git
+git clone https://github.com/justmat/massif.git
+git clone https://github.com/justmat/mangl.git
+git clone https://github.com/justmat/bounds.git
+git clone https://github.com/justmat/otis.git
+git clone https://github.com/justmat/pools.git
+git clone https://github.com/justmat/greyhole.git
+git clone https://github.com/justmat/compass.git
+git clone https://github.com/cfdrake/grendy.git
+git clone https://github.com/mimetaur/particle_grains.git
+git clone https://github.com/cfdrake/rudiments.git
+git clone https://github.com/21echoes/pedalboard.git
+
+echo "Hoorah!"
